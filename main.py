d16b975c2fdeb342545b01d6c4d5862385ddb464
Nirbhayjain8 <nirbhayjain8@gmail.com>
2022-09-28 21:57:17 +0200

    added main file

---
+++ main.py
@@ -0,0 +1,15 @@
+
+from sklearn.datasets import fetch_california_housing
+from sklearn.ensemble import RandomForestRegressor
+
+if __name__ == "__main__":
+    dataset  = fetch_california_housing()
+    features, labels = dataset["data"], dataset["target"]
+            
+    reg = RandomForestRegressor()
+    reg.fit(features, labels)
+    score = reg.score(features, labels)
+    print(f"Fitting score: {score}")
+    
+    print(f"Truth Label: {labels[0]}")
+    print(f"Predicted Label: {reg.predict(features[0].reshape(1, -1))}")
